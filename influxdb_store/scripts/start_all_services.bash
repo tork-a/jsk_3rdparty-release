1e21bc6a6b1cb737670cb994477373472ee36dca
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2023-06-08 20:02:29 +0900

    Trimmed the branch to only the 3rdparty/voice_text sub directory

--- influxdb_store/scripts/start_all_services.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-sudo systemctl start jsk-pr1012-influxdb.service
-sudo systemctl start jsk-pr1040-influxdb.service
-sudo systemctl start jsk-fetch15-influxdb.service
-sudo systemctl start jsk-fetch1075-influxdb.service
-set +x
