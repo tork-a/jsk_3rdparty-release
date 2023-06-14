7b2806f342052a9ddcd79e8845795154c1f2488e
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2023-06-14 14:03:33 +0900

    Trimmed the branch to only the webrtcvad_ros sub directory

--- influxdb_store/scripts/stop_all_services.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-sudo systemctl stop jsk-pr1012-influxdb.service
-sudo systemctl stop jsk-pr1040-influxdb.service
-sudo systemctl stop jsk-fetch15-influxdb.service
-sudo systemctl stop jsk-fetch1075-influxdb.service
-set +x
