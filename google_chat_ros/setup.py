2b143ca6db1eec4b7b4d7af89e74d7d77edd02a3
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2025-05-13 10:03:24 +0900

    Trimmed the branch to only the 3rdparty/ff sub directory

--- google_chat_ros/setup.py
+++
@@ -1,9 +0,0 @@
-from catkin_pkg.python_setup import generate_distutils_setup
-from setuptools import setup
-
-d = generate_distutils_setup(
-    packages=['google_chat_ros'],
-    package_dir={'': 'src'}
-)
-
-setup(**d)
