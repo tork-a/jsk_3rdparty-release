250a47b71bc80d779dd6f6bb4fb8007150514170
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2025-05-18 17:06:15 +0900

    Trimmed the branch to only the respeaker_ros sub directory

--- influxdb_store/python/influxdb_store/utils.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-import pytz
-
-
-def timestamp_to_influxdb_time(timestamp):
-    time = datetime.utcfromtimestamp(timestamp.to_sec())
-    time = pytz.timezone('UTC').localize(time).isoformat('T')
-    return time
--- rostwitter/python/rostwitter/__init__.py
+++
@@ -1,2 +0,0 @@
-from rostwitter.twitter import Twitter  # NOQA
-from rostwitter.util import load_oauth_settings  # NOQA
